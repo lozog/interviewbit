49dba1d76246fcd26c367b6f89f356391bdbe827
lozog <liam.ozog@gmail.com>
2018-07-29 21:13:58 -0400

    subtract: handle case where list length is 1

--- subtract/subtract.py
+++ subtract/subtract.py
@@ -8,7 +8,7 @@ class Solution:
 	# @param A : head node of linked list
 	# @return the head node in the linked list
 	def subtract(self, A):
-		if A == None:
+		if A == None or A.next == None:
 			# list is empty
 			return A
 
